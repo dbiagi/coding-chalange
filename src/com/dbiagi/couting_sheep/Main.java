5af64280cca1ade3d040b2a639330ef8dc5de2f2
Diego de Biagi <diegobiagiviana@gmail.com>
2017-03-27 22:34:23 -0300

    Counting sheep chalange.

---
+++ src/com/dbiagi/couting_sheep/Main.java
@@ -0,0 +1,103 @@
+package com.dbiagi.couting_sheep;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+/**
+ * Counting sheep problem.
+ *
+ * @see <a href="https://code.google.com/codejam/contest/6254486/dashboard">Problem link</a>
+ */
+public class Main {
+
+    private static String OUT_FILE_NAME = "saida";
+
+    public static void main(String[] args) throws IOException {
+        File file = null;
+
+        if (args.length > 0) {
+            file = new File(args[0]);
+        }
+
+        InputReader in;
+        PrintWriter out;
+
+        if (file != null && file.exists() && !file.isDirectory()) {
+            in = new InputReader(new FileInputStream(file));
+            out = new PrintWriter(new BufferedWriter(new FileWriter(OUT_FILE_NAME + ".out")));
+        } else {
+            in = new InputReader(System.in);
+            out = new PrintWriter(System.out, true);
+        }
+
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            String result = countSheeps(in.nextInt());
+
+            out.println(String.format("Case #%d: %s", i, result));
+        }
+
+        out.close();
+    }
+
+
+    private static String countSheeps(int n) {
+        if (n == 0) {
+            return "INSOMNIA";
+        }
+
+        Integer current = 0;
+        boolean seenAll = true;
+        HashSet<Integer> seen = new HashSet<>();
+
+        do {
+            current += n;
+
+            int aux = current;
+
+            while(aux > 0){
+                seen.add(aux%10);
+                aux /= 10;
+            }
+
+            seenAll = true;
+
+            for (int i = 0; i < 10; i++) {
+                if(!seen.contains(i)){
+                    seenAll = false;
+                    break;
+                }
+            }
+        } while (!seenAll);
+
+        return current.toString();
+    }
+
+    private static class InputReader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) throws FileNotFoundException {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = null;
+        }
+
+        private String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
